60297ae81d877705a3f0190d55ecd2175f650c29
IRSHADcreatelo <irshad.createlo@gmail.com>
2025-05-23 11:55:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -392,5 +392,5 @@ def _corsify_actual_response(response):
     return response
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=os.environ.get('DEBUG', 'False') == 'True')
